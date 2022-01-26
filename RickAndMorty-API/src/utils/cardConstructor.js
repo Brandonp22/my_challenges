f2619b316e53b54348fb5afade2e1ff1351ca252
Brandon Pineda <brandonpineda4@gmail.com>
2022-01-26 16:42:07 -0600

    Rick and morty finished

--- RickAndMorty-API/src/utils/cardConstructor.js
+++ RickAndMorty-API/src/utils/cardConstructor.js
@@ -6,21 +6,25 @@ const cardConstructor = character =>{
     card.innerHTML = `
 
     
-    <article>
-      <div>
-          <h3><span>${j}</span> ${character.name}</h3>
-          <span>${character.status}</span>
-      </div>
-      <div>
-          <img src="${character.image}" alt="">
-      </div>
-      <div>
-          <p>Especie: <span>${character.species}</span> </p>
-          <p>${character.gender}</p>
-          <p>${character.origin.name}</p>
-          <p>${character.location.name}</p>
-      </div>
-    </article>
+    <article class = "card-container">
+        <div class="card-content">
+            <div class = "card-title">
+                <h3><span>${j}</span> ${character.name}</h3>
+            </div>
+            <div class = "card-img">
+                <img src="${character.image}" alt="">
+                <div class = "card-status-container">
+                    <p class = "card-status status-${character.status}">${character.status}</p>
+                </div>
+            </div>
+            <div class = "card-info">
+                <p><i class="fas fa-atom"></i>Specie: <span>${character.species}</span></p>
+                <p><i class="fas fa-venus-mars"></i>Gender: <span>${character.gender}</span></p>
+                <p><i class="fas fa-globe"></i></i>Birthplace: <span>${character.origin.name}</span></p>
+                <p><i class="fas fa-map-marker-alt"></i>Last location: <span>${character.location.name}</span></p>
+            </div>
+        </div>
+      </article>
     
     `;
     return card;
